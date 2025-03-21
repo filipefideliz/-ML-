922f541bfa8b158498a1e9a601e03d8436d465bd
filipe cesar <lipe.puma@gmail.com>
2025-03-21 08:09:18 -0300

    feat= adicionando modelos de regressao linear

--- dia03/cerveja-notas.py
+++ dia03/cerveja-notas.py
@@ -16,6 +16,44 @@ plt.show()
 
 # %% 
 from sklearn import linear_model
+
 reg = linear_model.LinearRegression()
 reg.fit(df[['cerveja']], df['nota'])
 # %%
+# coef e o quanto vai aumentar a minha nota  o "b"
+# e o intercept  localização da nota e o "a"
+a , b = reg.intercept_, reg.coef_[0]
+print(f'a = {a} e b = {b}')
+# %%
+X = df[['cerveja']].drop_duplicates()
+y_estimado = reg.predict(X)
+y_estimado
+
+plt.plot(df['cerveja'], df['nota'], 'o')
+plt.plot(X, y_estimado, '-')
+plt.grid(True)
+plt.title('Relaçao Nota x Cerveja')
+plt.ylim(0,11)
+plt.xlim(0,11)
+plt.xlabel('Cerveja')
+plt.ylabel('Nota')
+plt.show()
+
+# %%
+from sklearn import tree
+arvore = tree.DecisionTreeRegressor(max_depth=2)
+arvore.fit(df[['cerveja']], df['nota'])
+
+y_estimado_arvore = arvore.predict(X)
+
+plt.plot(df['cerveja'], df['nota'], 'o')
+plt.plot(X, y_estimado, '-')
+plt.grid(True)
+plt.plot(X, y_estimado_arvore, '-')
+plt.title('Relaçao Nota x Cerveja')
+plt.ylim(0,11)
+plt.xlim(0,11)
+plt.xlabel('Cerveja')
+plt.ylabel('Nota')
+plt.legend(['Pontos','Regressao Linear', 'Arvore'])
+plt.show()
